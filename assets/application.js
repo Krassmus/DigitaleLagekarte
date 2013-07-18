50bacb8e79f48f72508940a0fc26148adc178891
Johannes Rudolph <johannes.rudolph@gmx.com>
2013-07-18 10:14:31 +0200

    draw:edited

--- assets/application.js
+++ assets/application.js
@@ -29,6 +29,13 @@ STUDIP.Lagekarte = {
 
             // Do whatever else you need to. (save to db, add to map etc)
             STUDIP.Lagekarte.map.addLayer(layer);
+        })
+
+        STUDIP.Lagekarte.map.on('draw:edited', function (e) {
+            var layers = e.layers;
+            layers.eachLayer(function (layer) {
+                //do whatever you want, most likely save back to db
+            });
         });
     },
     save_map: function () {

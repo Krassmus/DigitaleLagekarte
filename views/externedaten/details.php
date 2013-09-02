1e1e6c445024cc6a9156432c71b55b618faf5aea
Rasmus Fuhse <Krassmus@gmail.com>
2013-09-02 17:08:32 +0200

    add view file

---
+++ views/externedaten/details.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ *  Copyright (c) 2013  Rasmus Fuhse <fuhse@data-quest.de>
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
+ */
+
+function isAssoc($arr) {
+    return array_keys($arr) !== range(0, count($arr) - 1);
+}
+
+function display_json_representation($arr) {
+    $output = "";
+    if (isAssoc($arr)) {
+        $output .= "<ul>";
+        foreach ($arr as $key => $value) {
+            $output .= "<li data-key=\"".htmlReady($key)."\">";
+            if (is_array($value)) {
+                display_json_representation($value);
+            } else {
+                $output .= $value;
+            }
+            $output .= "</li>";
+        }
+        $output .= "</ul>";
+    } else {
+        $output .= "<ol>";
+        foreach ($arr as $value) {
+            $output .= "<li>";
+            if (is_array($value)) {
+                display_json_representation($value);
+            } else {
+                $output .= $value;
+            }
+            $output .= "</li>";
+        }
+        $output .= "</ol>";
+    }
+    return $output;
+}
+
+?>
+<h1><?= htmlReady($url['name']) ?></h1>
+<div class="json_object_list">
+    <pre><? var_dump($url) ?></pre>
+<?= display_json_representation($url['last_object']) ?>
+</div>

d364fc90be95b7b964cc9a0d0dcfa1441609ac4f
Rasmus Fuhse <krasmus@gmail.com>
2013-09-30 21:33:14 +0200

    better list design for external ressources

--- views/externedaten/details.php
+++ views/externedaten/details.php
@@ -16,18 +16,19 @@ function isAssoc($arr) {
 function display_json_representation($arr) {
     $output = "";
     if (isAssoc($arr)) {
-        $output .= "<ul>";
+        $output .= "<table><tbody>";
         foreach ($arr as $key => $value) {
-            $output .= "<li data-key=\"".htmlReady($key)."\">";
-            $output .= '<span class="key">'.htmlReady($key).'<span>: ';
+            $output .= "<tr data-key=\"".htmlReady($key)."\">";
+            $output .= '<td class="key">'.htmlReady($key).'</td>';
             if (is_array($value)) {
-                $output .= display_json_representation($value);
+                $output .= '<td>'.display_json_representation($value).'</td>';
             } else {
-                $output .= '<span class="value">'.htmlReady($value).'</span>';
+                $output .= '<td class="value">'.htmlReady($value).'</td>';
+                $output .= '<td class="match">'.Assets::img("icons/16/blue/staple", array('class' => "text-bottom")).'</td>';
             }
-            $output .= "</li>";
+            $output .= "</tr>";
         }
-        $output .= "</ul>";
+        $output .= "</tbody></table>";
     } else {
         $output .= "<ol>";
         foreach ($arr as $value) {
@@ -49,3 +50,21 @@ function display_json_representation($arr) {
 <div class="json_object_list">
 <?= display_json_representation($url['last_object']) ?>
 </div>
+
+
+<?
+$infobox = array(
+    array("kategorie" => _("Information"),
+          "eintrag"   =>
+        array(
+            array(
+                "icon" => "icons/16/black/tools",
+                "text" => _("Verknüpfen Sie einzelne Datensätze mit Attributen von Markern auf der Lagekarte. Sie werden dann regelmäßig automatisch aktualisiert.")
+            )
+        )
+    )
+);
+$infobox = array(
+    'picture' => $assets_url."pegelstaende.jpg",
+    'content' => $infobox
+);

e4fb26bcec9199241d896af1271a0801237002f1
Rasmus Fuhse <krasmus@gmail.com>
2013-10-28 22:23:26 +0100

    remove old eal.svg

--- views/schadenskonten/_poi_batch.php
+++ views/schadenskonten/_poi_batch.php
@@ -1,3 +1,3 @@
-<li id="poi_<?= $poi->getId() ?>" class="poi_batch" style="background-image: url('<?= $assets_url.($poi['image'] ? "markers".$poi["image"] : "eal.svg") ?>');">
+<li id="poi_<?= $poi->getId() ?>" class="poi_batch" style="background-image: url('<?= $assets_url.($poi['image'] ? "markers".$poi["image"] : "markers/struc/FlagGeneric.svg") ?>');">
     <div class="title"><?= htmlReady($poi['title']) ?> (<?= $poi['shape'] ?>)</div>
 </li>

0715348b74f937fc6b506661e51ea6200970436c
Rasmus Fuhse <krasmus@gmail.com>
2013-11-01 20:43:56 +0100

    don't overwrite last result with null

--- models/ExternalDataURL.class.php
+++ models/ExternalDataURL.class.php
@@ -52,14 +52,17 @@ class ExternalDataURL extends SimpleORMap {
         $result = curl_exec($c);
         $response_code = curl_getinfo($c, CURLINFO_HTTP_CODE);
         curl_close($c);
-        if ($result === false && ($object = studip_utf8decode(json_decode($result)))) {
+        if ($result === false) {
             curl_errno($c);
             curl_error($c);
         } else {
-            $this['last_object'] = $object;
-            $this['last_update'] = time();
-            $this->store();
-            $this->apply_mapping();
+            $object = studip_utf8decode(json_decode($result));
+            if ($object) {
+                $this['last_object'] = $object;
+                $this['last_update'] = time();
+                $this->store();
+                $this->apply_mapping();
+            }
         }
     }
     

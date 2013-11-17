cd859fd1ea88c555725a72d6d657cc066e4e9755
Rasmus Fuhse <krasmus@gmail.com>
2013-11-17 13:08:26 +0100

    bugfixing

--- models/ExternalDataURL.class.php
+++ models/ExternalDataURL.class.php
@@ -33,7 +33,7 @@ class ExternalDataURL extends SimpleORMap {
     }
 
     public function fetch() {
-        if (time() - $this['last_update'] >= $this['interval'] * 60) {
+        if ($this['last_update'] >= time() - ($this['interval'] * 60)) {
             return;
         }
         // cURL-Request:
